5f48a50e79ca58493a36133408fc69fe077428d9
torbite <89304412+torbite@users.noreply.github.com>
2024-05-15 15:51:32 -0300

    Update AI_Pong_Server.py

--- Piton/AI_Pong_Server.py
+++ Piton/AI_Pong_Server.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 import json
 from AI_Pong import AI
+import copy
 
 app = Flask(__name__)
 
@@ -13,22 +14,31 @@ def getAIAnswer(code):
     global AIsDict
     data = request.data
     inputs = json.loads(data)
-    result = AIsDict[int(code)].calculateAnswer(inputs)
+    result = AIsDict[int(code)][0].calculateAnswer(inputs)
     return f"{result}"
 
 @app.route("/getCode", methods = ["GET"])
 def getCode():
     global lastAiCode, AIsDict
     lastAiCode += 1
-    AIsDict[lastAiCode] = AI([15,7,3], 7)
+    AIsDict[lastAiCode] = [AI([15,7,3], 7), 0]
     print(AIsDict[lastAiCode].Ws)
     return str(lastAiCode)
 
 @app.route("/restart", methods = ["GET"])
 def randomizeAI():
     global AIsDict
+    best = AIsDict[1][1]
+    bestAI = AIsDict[1][0]
     for i in AIsDict.keys():
-        AIsDict[i].randomizeAI(10)
+        if AIsDict[i][1] > best:
+            bestAI = AIsDict[i][0]
+            best = AIsDict[i][1]
+    
+    for i in AIsDict.keys():
+        AIsDict[i][0] = copy.deepCopy(bestAI)
+        AIsDict[i][1] = 0
+        AIsDict[i][0].randomizeAI(10)
         
     return 'done'
 
